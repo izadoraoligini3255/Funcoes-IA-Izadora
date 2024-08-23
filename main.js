0e5c76a383f955702dbaca28533cb97e278f988f
izadora oligini <107197685+izadoraoligini3255@users.noreply.github.com>
2024-08-23 10:50:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,75 +1,92 @@
-const caixaPrincipal = dococument.querySelector(".caixa-principal");
-const caixaPergunta = dococument.querySelector(".caixa-pergunta");
-const caixaAlternativa = dococument.querySelector(".caixa-alternativa");
-const caixaResultado = dococument.querySelector(".caixa-resultado");
-const textoResultado = 
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "Quais materiais podem ser recicládos?",
         alternativas: [
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: " Plástico, papelão, vidro e alumínio",
+                afirmacao: " Parabéns você acertou esses são os lixos reciclávei",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Lampada, espelho, ampola de remedio e oculos",
+                afirmacao: " Que pena, você errou esses não são os lixos recicláveis",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Quais são os diferentes tipos de pláticos que podem ser reciclados?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Polipropileno (cobre), polietileno (garrafas de água)",
+                afirmacao: "Esses são os plástico corretos para fazer a reciclágem ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Plástico PVC e ácido polilatico",
+                afirmacao: "Esses não são plásticos adequados para a areciclágem",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "O que é coleta seletiva?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "É a separação do lixo em devidas cores, separando o organico de plásticos e recicláveis",
+                afirmacao: "Essa é a forma correta de separação",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "São lixos que podem ser misturados",
+                afirmacao: "São formas erradas de coleta seletiva",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "O que são resíduos líquidos?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "São materiais não aproveitados que se encontram no estado líquido",
+                afirmacao: "Parabéns, você acertouessa é a afirmativa correta",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "São materiais recicláveis que podem ser reutilizados",
+                afirmacao: "Que pena, essa não é a resposta corta",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: "Onde é o lugar correto para descartar os lixos?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: "Afirmação",
+                texto: "Em çixeiras proximas para que seja dirigida aos lixôes",
+                afirmacao: "Esa é a reposta certa, parabéns",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Nas ruas ou em qualquer lugar onde estiver",
+                afirmacao: "Infelizmente não é a forma correta",
             }
         ]
     },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
